a959a4a08d0d8b91307bf42e8680c409e1837ad1
LouviersJ <lou.joseph@students.fhu.edu>
2024-01-31 13:59:07 -0600

    Yippee

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-const facts = require('./facts.json')
+const facts = require('./facts.json');
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const PORT = process.env.PORT || "3000"
+const PORT = process.env.PORT || "3000";
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.listen(PORT, ()=> {
-    console.log( `App is running on http://localhost:${PORT}...`)
-})
+app.listen(PORT, ()=> {console.log( `App is running on http://localhost:${PORT}...`)});
 
 app.get("/", (req, res) => {
-
-    res.send("Good Job!")
-
-})
+    res.send("Coding hard in the cubicles")
+});
 
 // http://localhost:3000/greet?name=kaylee&dob=2002
 app.get('/greet', (req,res)=>{
@@ -27,7 +23,7 @@ app.get('/greet', (req,res)=>{
 app.get('/math/:number/:symbol/:number_', (req, res)=> {
     const number = parseInt(req.params.number)
     const number_ = parseInt(req.params.number_)
-    const symbol = req.params.symbol.toLowerCase();
+    const symbol = req.params.symbol.toLowerCase()
 
     switch(symbol){
         case "plus":
@@ -46,10 +42,16 @@ app.get('/math/:number/:symbol/:number_', (req, res)=> {
 });
 
 app.get('/pandorasbox', (req,res)=>{
-    
     const length = facts.length
     const random = Math.floor(Math.random()*length)
     const fact = facts[random].fact
-    res.render('pandorasbox', {title: "Pandora's box", message: fact})
+    const rand = Math.floor(Math.random()*100)
     
+    if( rand%2==0 ){
+    fetch("https://icanhazdadjoke.com",{ headers:{"Accept": "application/json"}})
+    .then(res => res.json())
+    .then((data) => {res.render('pandorasbox',{title:"Pandora's box", message: data.joke})})
+    }else{
+    res.render('pandorasbox', {title: "Pandora's box", message: fact})
+    }
 });
